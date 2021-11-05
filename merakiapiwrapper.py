9af7cfd3e0337c44bb5b3b20dd526969120597c1
Jason Chambers (jaschamb) <jaschamb@cisco.com>
2021-11-05 16:33:29 -0400

    Calls to Meraki dashboard no longer working

--- merakiapiwrapper.py
+++ merakiapiwrapper.py
@@ -11,13 +11,11 @@ class MerakiApiWrapper():
         self.network_id = network_id
         self.dashboard = meraki.DashboardAPI(
             api_key=api_key,
-            base_url='https://api-mp.meraki.com/api/v0/',
             output_log=False)
 
     def get_rules(self):
         """Get the rules from the firewall."""
-        mx_l3_firewall = self.dashboard.mx_l3_firewall
-        return mx_l3_firewall.getNetworkL3FirewallRules(self.network_id)
+        return self.dashboard.appliance.getNetworkApplianceFirewallL3FirewallRules(self.network_id)
 
     def update_rules(self, rules):
         """Upddate the rules in the firewall."""
